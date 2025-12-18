dfa2af86dca19c33ba5cdf54b2aaa8271439ea75
DCodeProg <danael.legrand@outlook.fr>
2025-12-18 10:10:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# health_ia_care_app
+
+AI coach app
---
+++ android/app/src/main/kotlin/com/example/health_ia_care_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.health_ia_care_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
