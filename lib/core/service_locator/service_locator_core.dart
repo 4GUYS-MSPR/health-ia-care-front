4faf8dfa49eb617723db530bbb6a1c490d6052b5
DCodeProg <danael.legrand@outlook.fr>
2026-01-24 20:26:02 +0100

    feat: add logger initialization in service locator for enhanced logging setup

--- lib/core/service_locator/service_locator_core.dart
+++ lib/core/service_locator/service_locator_core.dart
@@ -2,11 +2,13 @@ import 'package:get_it/get_it.dart';
 
 import 'init/dotenv_init.dart';
 import 'init/hydrated_storage_init.dart';
+import 'init/logger_init.dart';
 import 'modules/cubits_module.dart';
 
 Future<void> registerCoreDependencies(GetIt sl) async {
   // Inits
   await initHydratedStorage();
+  await initLogger(sl);
   await initDotenv();
 
   // Modules
