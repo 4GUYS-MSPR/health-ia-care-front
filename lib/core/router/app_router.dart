d8e95ba37ab3efadf747da1b70cf240419196823
Théo danel <theo.danel@ecoles-epsi.net>
2026-01-25 15:45:25 +0100

    feat: create AppRouter class for managing application routes

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:go_router/go_router.dart';
+
+import '../shared/pages/home_page.dart';
+import 'app_routes.dart';
+
+class AppRouter {
+  GoRouter get router => _goRouter;
+
+  final _goRouter = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        name: AppRoutes.home,
+        builder: (context, state) => HomePage(),
+      ),
+    ],
+  );
+}
