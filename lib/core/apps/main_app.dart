e98a823164531c5f86174de7e014e969b8065bb3
Theo Danel <136567176+theodanel@users.noreply.github.com>
2026-01-25 22:25:45 +0100

    refactor: improve main.dart logic (#25)
    * feat: add MainApp widget with MultiBlocProvider and router configuration
    
    * refactor: streamline app initialization and logging structure
    
    * fix: resolve merge conflict in pubspec.lock
    
    ---------
    
    Co-authored-by: DCodeProg <danael.legrand@outlook.fr>

---
+++ lib/core/apps/main_app.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../l10n/generated/app_localizations.dart';
+import '../extensions/l10n_extension.dart';
+import '../router/app_router.dart';
+import '../service_locator/service_locator.dart';
+import '../shared/cubits/locale_cubit/locale_cubit.dart';
+import '../shared/cubits/theme_cubit/theme_cubit.dart';
+import '../theme/app_theme.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => sl<ThemeCubit>()),
+        BlocProvider(create: (_) => sl<LocaleCubit>()),
+      ],
+      child: Builder(
+        builder: (context) {
+          final themeMode = context.watch<ThemeCubit>().state;
+          final selectedLocale = context.watch<LocaleCubit>().state;
+          final appRouter = sl<AppRouter>();
+
+          return MaterialApp.router(
+            // App title
+            onGenerateTitle: (context) => context.l10n.appTitle,
+
+            // Theme related
+            theme: AppTheme.lightMediumContrast,
+            darkTheme: AppTheme.dark,
+            highContrastTheme: AppTheme.lightHighContrast,
+            highContrastDarkTheme: AppTheme.darkHighContrast,
+            themeMode: themeMode,
+
+            // l10n related
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            locale: selectedLocale,
+
+            // Router related
+            routerConfig: appRouter.router,
+          );
+        },
+      ),
+    );
+  }
+}
