727aeabef832d1d33c69c0677894f56b482686fe
DCodeProg <danael.legrand@outlook.fr>
2026-01-26 22:53:41 +0100

    refactor: remove InternetConnectionChecker from service locator

--- lib/core/service_locator/modules/network_module.dart
+++ lib/core/service_locator/modules/network_module.dart
@@ -4,11 +4,9 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 import '../../network/network_info.dart';
 
 void registerNetwork(GetIt sl) {
-  sl.registerLazySingleton<InternetConnectionChecker>(
-    () => InternetConnectionChecker.createInstance(),
-  );
+  final connectionChecker = InternetConnectionChecker.createInstance();
 
   sl.registerLazySingleton<NetworkInfo>(
-    () => NetworkInfoImpl(connectionChecker: sl<InternetConnectionChecker>()),
+    () => NetworkInfoImpl(connectionChecker: connectionChecker),
   );
 }
