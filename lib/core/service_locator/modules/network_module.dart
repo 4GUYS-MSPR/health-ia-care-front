d466f2b1f0d8bddd69892a7055c1a7258d302aa2
DCodeProg <130582816+DCodeProg@users.noreply.github.com>
2026-01-26 22:57:26 +0100

    feat: network module (#28)
    * feat: add abstract interface for network connectivity
    
    * build(pubspec): add internet_connection_checker to dependencies
    
    * feat: implement NetworkInfo interface
    
    * feat: add network module registration to service locator
    
    * feat: add unit tests for NetworkInfo implementation
    
    * refactor: remove InternetConnectionChecker from service locator

---
+++ lib/core/service_locator/modules/network_module.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import '../../network/network_info.dart';
+
+void registerNetwork(GetIt sl) {
+  final connectionChecker = InternetConnectionChecker.createInstance();
+
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(connectionChecker: connectionChecker),
+  );
+}
