bca11c1666d0346b342c4002a02aa978d27ebb7f
DCodeProg <danael.legrand@outlook.fr>
2026-01-29 22:37:57 +0100

    feat(auth): add AuthSession entity for authentication handling

---
+++ lib/features/authentication/domain/entities/auth_reponse.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+class AuthResponse extends Equatable {
+  final String token;
+
+  const AuthResponse({
+    required this.token,
+  });
+
+  @override
+  List<Object?> get props => [
+    token,
+  ];
+}
