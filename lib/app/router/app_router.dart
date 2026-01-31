ca5620b9a5a90819cdf96d9f92ec45d177160263
DCodeProg <danael.legrand@outlook.fr>
2026-01-31 19:19:18 +0100

    feat(auth): update routing to include login page and protected route

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:go_router/go_router.dart';
 
 import '../../core/shared/pages/home_page.dart';
+import '../../features/authentication/presentation/pages/login_page.dart';
 import 'app_routes.dart';
+import 'protected_go_route.dart';
 
 class AppRouter {
   GoRouter get router => _goRouter;
@@ -9,10 +11,15 @@ class AppRouter {
   final _goRouter = GoRouter(
     initialLocation: '/',
     routes: [
-      GoRoute(
+      ProtectedGoRoute(
         path: '/',
         name: AppRoutes.home,
         builder: (context, state) => HomePage(),
+      ),
+      GoRoute(
+        path: '/login',
+        name: AppRoutes.login,
+        builder: (context, state) => LoginPage(),
       ),
     ],
   );
