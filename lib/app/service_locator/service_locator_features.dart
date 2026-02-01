85aaeaf9a0e2c03759da3c590e360ef594be11fd
DCodeProg <danael.legrand@outlook.fr>
2026-02-01 19:46:06 +0100

    feat: add AuthInterceptor for automatic token handling in API requests

--- lib/app/service_locator/service_locator_features.dart
+++ lib/app/service_locator/service_locator_features.dart
@@ -2,8 +2,10 @@ import 'package:get_it/get_it.dart';
 
 import '../../features/authentication/di/authentication_injection.dart';
 import '../../features/health/di/health_injection.dart';
+import '../../features/members/di/members_injection.dart';
 
 Future<void> registerFeatureDependencies(GetIt sl) async {
   registerAuthenticationFeature(sl);
   registerNutritionFeature(sl);
+  registerMembersFeature(sl);
 }
