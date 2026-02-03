80d3632bc48958078dfa38c506992f86c8540def
Théo danel <theo.danel@ecoles-epsi.net>
2026-02-03 14:35:48 +0100

    fix: update NutritionFailure to use debugMessage instead of message

--- lib/features/health/domain/errors/nutrition_failure.dart
+++ lib/features/health/domain/errors/nutrition_failure.dart
@@ -1,15 +1,15 @@
 import '../../../../core/errors/failures.dart';
 
 abstract class NutritionFailure extends Failure {
-  const NutritionFailure({required super.message});
+  const NutritionFailure({required super.debugMessage});
 }
 
 class NutritionConnectionFailure extends NutritionFailure {
   const NutritionConnectionFailure()
-      : super(message: "Pas de connexion internet");
+      : super( debugMessage: "Pas de connexion internet");
 }
 
 class NutritionServerFailure extends NutritionFailure {
   const NutritionServerFailure()
-      : super(message: "Erreur serveur");
+      : super( debugMessage: "Erreur serveur");
 }
